3e89cebb3bc301dff005d14b343f436ac0bfe75b
iorise <riofirmansyah1805@gmail.com>
2023-06-22 11:58:29 +0700

    add valid functionality

--- components/addNewTaskModal.tsx
+++ components/addNewTaskModal.tsx
@@ -1,51 +1,81 @@
 "use client";
 import { VscAdd } from "@/assets/icons";
-import { useState } from "react";
-import { push, set } from "firebase/database"
+import { useEffect, useState } from "react";
 import useCreateTasks from "@/hooks/useCreateTasks";
-import { title } from "process";
 
-
-
-export default function AddNewTask(){
-  const createTasks = useCreateTasks()
+export default function AddNewTask() {
+  const createTasks = useCreateTasks();
   const [modal, setModal] = useState(false);
-  const [loading, setLoading] = useState(false)
-  const [title, setTitle] = useState("")
-  const [description, setDescription] = useState("")
-  const [date, setDate] = useState("")
-  const [priority, setPriority] = useState("low")
+  const [loading, setLoading] = useState(false);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [date, setDate] = useState("");
+  const [priority, setPriority] = useState("low");
+  const [formValid, setFormValid] = useState(false);
+  const [titleValid, setTitleValid] = useState(true);
+  const [descriptionValid, setDescriptionValid] = useState(true);
+  const [dateValid, setDateValid] = useState(true);
 
   const resetForm = () => {
     setTitle("");
     setDescription("");
     setDate("");
     setPriority("low");
+    setTitleValid(true);
+    setDescriptionValid(true);
+    setDateValid(true);
   };
-  
 
   const createTask = async () => {
-    const path = "/tasks"
+    const path = "/tasks";
     const value = {
       title: title,
       description: description,
       date: date,
-      priority: priority
-    }
-
-    await createTasks.pushTask(path, value)
-    setModal(false)
-    resetForm()
-  }
-
+      priority: priority,
+    };
+    setLoading(true);
+    await createTasks.pushTask(path, value);
+    setModal(false);
+    resetForm();
+    setLoading(false);
+  };
 
   function handleChange() {
     setModal(!modal);
-    resetForm()
+    resetForm();
+    setFormValid(false);
   }
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+
+    if (!title) {
+      setTitleValid(false);
+    } else {
+      setTitleValid(true);
+    }
+
+    if (!description) {
+      setDescriptionValid(false);
+    } else {
+      setDescriptionValid(true);
+    }
+
+    if (!date) {
+      setDateValid(false);
+    } else {
+      setDateValid(true);
+    }
+
+    if (title && description && date) {
+      setFormValid(true);
+      createTask();
+    } else {
+      setFormValid(false);
+      return;
+    }
   };
+
   return (
     <div>
       <button onClick={handleChange}>
@@ -66,31 +96,63 @@ export default function AddNewTask(){
                 <label className="label text-lg">Title</label>
                 <input
                   type="text"
-                  className="input input-bordered"
+                  className={`input input-bordered ${
+                    !titleValid ? "input-error" : ""
+                  }`}
                   placeholder="Title"
                   value={title}
-                  onChange={(e) => setTitle(e.target.value)}
+                  onChange={(e) => {
+                    setTitle(e.target.value);
+                    setTitleValid(true);
+                  }}
                 />
+                {!titleValid && (
+                  <p className="text-red-500 text-sm font-light mt-1">
+                    please provide a title.
+                  </p>
+                )}
                 <label className="label text-lg">Description</label>
                 <textarea
-                  className="input input-bordered h-32"
+                  className={`input input-bordered h-32 ${
+                    !descriptionValid ? "input-error" : ""
+                  }`}
                   placeholder="Desc"
                   value={description}
-                  onChange={(e) => setDescription(e.target.value)}
+                  onChange={(e) => {
+                    setDescription(e.target.value);
+                    setDescriptionValid(true);
+                  }}
                 ></textarea>
+                {!descriptionValid && (
+                  <p className="text-red-500 text-sm font-light mt-1">
+                    Please provide a description.
+                  </p>
+                )}
               </div>
               <div className="right">
                 <label className="label text-lg">Date</label>
                 <input
                   type="date"
-                  className="input input-bordered"
+                  className={`input input-bordered ${
+                    !dateValid ? "input-error" : ""
+                  }`}
                   placeholder="date"
                   value={date}
-                  onChange={(e) => setDate(e.target.value)}
+                  onChange={(e) => {
+                    setDate(e.target.value);
+                    setDateValid(true);
+                  }}
                 />
+                {!dateValid && (
+                  <p className="text-red-500 text-sm font-light mt-1">
+                    Please provide a date.
+                  </p>
+                )}
                 <label className="label text-lg">Priority</label>
                 <select
-                onChange={(e) => setPriority(e.target.value)} className="input input-bordered">
+                  onChange={(e) => setPriority(e.target.value)}
+                  className="input input-bordered"
+                >
                   <option value="low">Low</option>
                   <option value="medium">Medium</option>
                   <option value="high">High</option>
@@ -104,7 +166,9 @@ export default function AddNewTask(){
             <button className="btn" onClick={handleChange}>
               Close
             </button>
-            <button type="submit" className="btn" onClick={createTask} >Add Task</button>
+            <button type="submit" className="btn" onClick={handleSubmit}>
+              Add Task
+            </button>
           </div>
         </div>
       </div>
