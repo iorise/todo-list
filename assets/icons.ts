32b563830bdb2b3f25f5944ea5f5f6c8b43d12d3
iorise <riofirmansyah1805@gmail.com>
2023-06-23 21:20:48 +0700

    add icon

--- assets/icons.ts
+++ assets/icons.ts
@@ -1,3 +1,6 @@
 export { GiHamburgerMenu } from "react-icons/gi";
 export { AiOutlineHome } from "react-icons/ai";
 export { VscAdd } from "react-icons/vsc";
+export { IoWarningOutline, IoTodayOutline } from "react-icons/io5"
+export { BsInbox } from "react-icons/bs"
+export { BiCalendarWeek } from "react-icons/bi"
